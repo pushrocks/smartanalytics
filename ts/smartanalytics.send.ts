c1113dc257f4ef10cc0af198c7a83722b8f46642
Phil Kunz <gitlab@philkunz.com>
2017-02-24 03:06:51 +0100

    fix indention

--- ts/smartanalytics.send.ts
+++ ts/smartanalytics.send.ts
@@ -1,22 +1,22 @@
 import * as plugins from './smartanalytics.plugins'
 
 export let event = (trackingIdArg: string, appNameArg: string, eventCategoryArg: string, eventActionArg, eventLabelArg: string = 'null') => {
-    let payload: string = 'v=1' +
-        `&tid=${trackingIdArg}` + // the tracking ID
-        '&cid=555' +
-        '&aip=1' + // anonymize the IP
-        '&t=event' +
-        `&ec=${eventCategoryArg}` +
-        `&ea=${eventActionArg}` +
-        `&el=${eventLabelArg}` +
-        '&ev=300' +
-        `&an=${eventLabelArg}`
-    send(payload)
+  let payload: string = 'v=1' +
+    `&tid=${trackingIdArg}` + // the tracking ID
+    '&cid=555' +
+    '&aip=1' + // anonymize the IP
+    '&t=event' +
+    `&ec=${eventCategoryArg}` +
+    `&ea=${eventActionArg}` +
+    `&el=${eventLabelArg}` +
+    '&ev=300' +
+    `&an=${eventLabelArg}`
+  send(payload)
 }
 
 
 let send = async (requestBodyArg: string) => {
-    await plugins.smartrequest.post('https://www.google-analytics.com/collect', {
-        requestBody: requestBodyArg
-    })
+  await plugins.smartrequest.post('https://www.google-analytics.com/collect', {
+    requestBody: requestBodyArg
+  })
 }
